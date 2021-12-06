52a15fe42c8b41e7d8bb2e165f9486621336191c
ohshyuk5 <seunghyukoh0@gmail.com>
2021-12-07 00:43:57 +0900

    Update - Temporal - Request my wallet data from upbit with token

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -18,6 +21,26 @@ func main() {
 	secretKey := os.Getenv("SECRET_KEY")
 
 	token := myJwt.CreateJwtToken(accessKey, secretKey, "")
-	fmt.Println(token)
+
+	url := "https://api.upbit.com/v1/accounts"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	var data string
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+	fmt.Println(data)
 
 }
