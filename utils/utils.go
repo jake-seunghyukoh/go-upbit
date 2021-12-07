09405fa0e6a4c60ffb4e9754a616ec134388e042
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-07 16:47:00 +0900

    Update - Http error handler - Log err body

--- utils/utils.go
+++ utils/utils.go
@@ -1,16 +1,34 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type errorMessage struct {
+	Name    int    `json:"name"`
+	Message string `json:"message"`
+}
+
+type errorBody struct {
+	Error errorMessage `json:"error"`
+}
 
 func HandleHttpError(response *http.Response, err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 	if response.StatusCode >= 400 {
-		log.Fatal(fmt.Sprintf("Http request error with code:%d", response.StatusCode))
+		defer response.Body.Close()
+
+		var errorBody errorBody
+		errDecode := json.NewDecoder(response.Body).Decode(&errorBody)
+		if errDecode != nil {
+			log.Fatal("Http request decoding error")
+		}
+
+		log.Fatal(fmt.Sprintf("Http request error with code:%d\n%v", response.StatusCode, errorBody))
 	}
 }
